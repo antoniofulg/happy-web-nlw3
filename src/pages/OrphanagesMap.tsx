4e91ec5635f36b00ed56f47dd89a79f2187594b8
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-10-17 01:08:27 -0300

    feature: added leaflet map

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
+
+import mapMarkerImg from '../images/map-marker.svg'
+
+import '../styles/pages/orphanages-map.css'
 
 function OrphanagesMap() {
   return (
-    <h1>Hello world</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+          
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Crato</strong>
+          <span>Ceará</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-7.2218099, -39.3932224]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+
+    </div>
   )
 }
 
